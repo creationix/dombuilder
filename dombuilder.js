b72c64bb79d2918bd72abd611084eaae2c365f15
Tim Caswell <tim@creationix.com>
2012-07-26 23:32:05 -0500

    Package for jamjs, use strict, and fix typo

--- dombuilder.js
+++ dombuilder.js
@@ -6,81 +6,88 @@
 //                                  //
 //////////////////////////////////////
 
-(function () {
-  this.domBuilder = function domBuilder(json, refs) {
+( // Module boilerplate to support browser globals and AMD.
+  (typeof define === "function" && function (m) { define("dombuilder", m); }) ||
+  (function (m) { window.domBuilder = m(); })
+)(function () {
+"use strict";
 
-    // Render strings as text nodes
-    if (typeof json === 'string') return document.createTextNode(json);
+function domBuilder(json, refs) {
 
-    var node, first;
-    for (var i = 0, l = json.length; i < l; i++) {
-      var part = json[i];
+  // Render strings as text nodes
+  if (typeof json === 'string') return document.createTextNode(json);
 
-      if (!node) {
-        if (typeof part === 'string') {
-          // Create a new dom node by parsing the tagline
-          var tag = part.match(TAG_MATCH);
-          tag = tag ? tag[0] : "div";
-          node = document.createElement(tag);
-          first = true;
-          var classes = part.match(CLASS_MATCH);
-          if (classes) node.setAttribute('class', classes.map(stripFirst).join(' '));
-          var id = part.match(ID_MATCH);
-          if (id) node.setAttribute('id', id[0].substr(1));
-          var ref = part.match(REF_MATCH);
-          if (refs && ref) refs[ref[0].substr(1)] = node;
-          continue;
-        } else {
-          node = document.createDocumentFragment();
-        }
-      }
+  var node, first;
+  for (var i = 0, l = json.length; i < l; i++) {
+    var part = json[i];
 
-      // Except the first item if it's an attribute object
-      if (first && typeof part === 'object' && part.__proto__ === Object.prototype) {
-        setAttrs(node, part);
+    if (!node) {
+      if (typeof part === 'string') {
+        // Create a new dom node by parsing the tagline
+        var tag = part.match(TAG_MATCH);
+        tag = tag ? tag[0] : "div";
+        node = document.createElement(tag);
+        first = true;
+        var classes = part.match(CLASS_MATCH);
+        if (classes) node.setAttribute('class', classes.map(stripFirst).join(' '));
+        var id = part.match(ID_MATCH);
+        if (id) node.setAttribute('id', id[0].substr(1));
+        var ref = part.match(REF_MATCH);
+        if (refs && ref) refs[ref[0].substr(1)] = node;
+        continue;
       } else {
-        node.appendChild(domBuilder(part, refs));
-      }
-      first = false;
-    }
-    return node;
-  };
-
-  function setAttrs(node, attrs) {
-    var keys = Object.keys(attrs);
-    for (var i = 0, l = keys.length; i < l; i++) {
-      var key = keys[i];
-      var value = attrs[key];
-      if (key === "$") {
-        value(node);
-      } else if (key === "css") {
-        setStyle(node.style, value);
-      } else if (key.substr(0, 2) === "on") {
-        node.addEventListener(key.substr(2), value, false);
-      } else {
-        node.setAttribute(key, value);
+        node = document.createDocumentFragment();
       }
     }
-  }
 
-  function setStyle(style, attrs) {
-    var keys = Object.keys(attrs);
-    for (var i = 0, l = keys.length; i < l; i++) {
-      var key = keys[i];
-      style[key] = attrs[key];
+    // Except the first item if it's an attribute object
+    if (first && typeof part === 'object' && part.__proto__ === Object.prototype) {
+      setAttrs(node, part);
+    } else {
+      node.appendChild(domBuilder(part, refs));
+    }
+    first = false;
+  }
+  return node;
+};
+
+function setAttrs(node, attrs) {
+  var keys = Object.keys(attrs);
+  for (var i = 0, l = keys.length; i < l; i++) {
+    var key = keys[i];
+    var value = attrs[key];
+    if (key === "$") {
+      value(node);
+    } else if (key === "css") {
+      setStyle(node.style, value);
+    } else if (key.substr(0, 2) === "on") {
+      node.addEventListener(key.substr(2), value, false);
+    } else {
+      node.setAttribute(key, value);
     }
   }
+}
 
-  var CLASS_MATCH = /\.[^.#$]+/g,
-      ID_MATCH = /#[^.#$]+/,
-      REF_MATCH = /\$[^.#$]+/;
-      TAG_MATCH = /^[^.#$]+/;
-
-  function stripFirst(part) {
-    return part.substr(1);
+function setStyle(style, attrs) {
+  var keys = Object.keys(attrs);
+  for (var i = 0, l = keys.length; i < l; i++) {
+    var key = keys[i];
+    style[key] = attrs[key];
   }
+}
 
-}());
+var CLASS_MATCH = /\.[^.#$]+/g,
+    ID_MATCH = /#[^.#$]+/,
+    REF_MATCH = /\$[^.#$]+/,
+    TAG_MATCH = /^[^.#$]+/;
+
+function stripFirst(part) {
+  return part.substr(1);
+}
+
+return domBuilder;
+
+});
 
 
 
